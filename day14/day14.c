e34c24f52acf5fcdd87b50be4acd2a86c878e77c
Neil Gall <neil@gall.scot>
2021-12-14 23:51:05 +0000

    Day 14 part 2

--- day14/day14.c
+++ day14/day14.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,14 +9,15 @@
 struct instructions {
     size_t template_length;    
     char template[32];
-    char rules[26*26];
+    char rules[26][26];
 };
 
 struct instructions *parse(struct Reader *reader) {
     struct instructions *i = (struct instructions *)malloc(sizeof(struct instructions));
     memset(i, 0, sizeof(struct instructions));
     int state = 0;
-    size_t rule_index = 0;
+    int rule_left = 0;
+    int rule_right = 0;
     char c;
     while ((c = reader->next(reader)) != 0) {
         switch (state) {
@@ -28,19 +30,19 @@ struct instructions *parse(struct Reader *reader) {
                 break;
             case 1:
                 if (isalpha(c)) {
-                    rule_index = (c - 'A') * 26;
+                    rule_left = c - 'A';
                     state = 2;
                 }
                 break;
             case 2:
                 if (isalpha(c)) {
-                    rule_index += (c - 'A');
+                    rule_right = c - 'A';
                     state = 3;
                 }
                 break;
             case 3:
                 if (isalpha(c)) {
-                    i->rules[rule_index] = c;
+                    i->rules[rule_left][rule_right] = c;
                     state = 1;
                 }
                 break;
@@ -51,75 +53,96 @@ struct instructions *parse(struct Reader *reader) {
     return i;
 }
 
+void template_pair(struct instructions *i, size_t index, int *left, int *right) {
+    *left = i->template[index] - 'A';
+    *right = i->template[index+1] - 'A';
+}
+
+typedef long count_t;
+
 struct polymer {
-    size_t capacity;
-    size_t length;
-    char elements[];
+    count_t counts[26][26];
 };
 
-struct polymer *new_polymer(size_t capacity) {
-    size_t bytes = sizeof(struct polymer) + capacity;
-    struct polymer *p = (struct polymer *)malloc(bytes);
-    memset(p, 0, bytes);
-    p->capacity = capacity;
-    return p;
+void polymer_from_template(struct instructions *i, struct polymer *p) {
+    memset(p, 0, sizeof(struct polymer));
+
+    for (size_t e = 0; e < i->template_length-1; ++e) {
+        int left, right;
+        template_pair(i, e, &left, &right);
+        p->counts[left][right]++;
+    }
 }
 
-struct polymer *polymer_from_template(struct instructions *i) {
-    struct polymer *p = new_polymer(i->template_length);
-    p->length = i->template_length;
-    memcpy(p->elements, i->template, i->template_length);
-    return p;
+void print_pair_counts(struct polymer *p) {
+    for (int a = 0; a < 26; ++a)
+        for (int b = 0; b < 26; ++b)
+            if (p->counts[a][b])
+                printf("%c%c: %ld\n", a+'A', b+'A', p->counts[a][b]);
 }
 
-void append_polymer(struct polymer *p, char c) {
-    assert(p->length < p->capacity);
-    p->elements[p->length++] = c;
-}
 
-struct polymer *grow_polymer(struct polymer *p, struct instructions *i) {
-    struct polymer *new = new_polymer(p->length * 2);
-    for (size_t e = 0; e < p->length-1; ++e) {
-        append_polymer(new, p->elements[e]);
-        size_t rule_index = (p->elements[e] - 'A') * 26 + p->elements[e+1] - 'A';
-        char insert = i->rules[rule_index];
-        if (isalpha(insert)) {
-            append_polymer(new, insert);
+void grow_polymer(struct instructions *i, struct polymer *p) {
+    count_t new[26][26] = { 0 };
+
+    for (size_t left = 0; left < 26; ++left) {
+        for (size_t right = 0; right < 26; ++right) {
+            char insert = i->rules[left][right];
+            count_t pair_count = p->counts[left][right];
+            if (isalpha(insert) && pair_count > 0) {
+                // printf("insert %c between %c,%c: %ld\n", insert, left+'A', right+'A', pair_count);
+                insert -= 'A';
+                new[left][insert] += pair_count;
+                new[insert][right] += pair_count;
+            } else if (pair_count > 0) {
+                // printf("%c,%c unchanged %ld\n", left+'A', right+'A', pair_count);
+                new[left][right] += pair_count;
+            }
         }
     }
-    append_polymer(new, p->elements[p->length-1]);
-    return new;
+
+    memcpy(&p->counts, &new, sizeof(new));
+    // print_pair_counts(p);
 }   
 
-int compare_ints(const void *a, const void *b) {
-    return *(int *)a - *(int *)b;
-}
-
-int part1(struct instructions *instr) {
+count_t run_polymer_growth(struct instructions *instr, int steps) {
 
     // run polymer growth 10 times
-    struct polymer *p = polymer_from_template(instr);
-    for (int i = 0; i < 10; ++i) {
-        struct polymer *new = grow_polymer(p, instr);
-        free(p);
-        p = new;
+    struct polymer p;
+    polymer_from_template(instr, &p);
+
+    for (int i = 0; i < steps; ++i) {
+        grow_polymer(instr, &p);
     }
 
-    // count frequencies of each element
-    int freq[26] = { 0 };
-    for (size_t i = 0; i < p->length; ++i) {
-        freq[p->elements[i] - 'A']++;
+    // count frequencies of each element from the first of each pair
+    count_t freq[26] = { 0 };
+    for (int a = 0; a < 26; ++a) {
+        for (int b = 0; b < 26; ++b) {
+            freq[a] += p.counts[a][b];
+        }
     }
-    qsort(freq, 26, sizeof(int), compare_ints);
-    
+    freq[instr->template[instr->template_length-1] - 'A'] += 1; // last element
+
     // find most and least frequent
-    size_t min = 0;
-    while (min < 26 && freq[min] == 0) min++;
-    size_t max = 25;
-    while (max > 0 && freq[max] == 0) max--;
+    count_t min = LONG_MAX;
+    count_t max = freq[0];
+    for (int a = 1; a < 26; ++a) {
+        if (freq[a] != 0 && freq[a] < min)
+            min = freq[a];
+        if (freq[a] > max)
+            max = freq[a];
+    }
 
-    free(p);
-    return freq[max] - freq[min];
+    return max - min;
+}
+
+count_t part1(struct instructions *i) {
+    return run_polymer_growth(i, 10);
+}
+
+count_t part2(struct instructions *i) {
+    return run_polymer_growth(i, 40);
 }
 
 static const char test_data[] =
@@ -145,8 +168,9 @@ void tests() {
     struct instructions *i = parse(string_reader(test_data));
     assert(i->template_length == 4);
     assert(strcmp(i->template, "NNCB") == 0);
-    assert(i->rules[('C'-'A')*26 + 'H'-'A'] == 'B');
+    assert(i->rules['C'-'A']['H'-'A'] == 'B');
     assert(part1(i) == 1588);
+    assert(part2(i) == 2188189693529);
     free(i);
 }
 
@@ -154,7 +178,8 @@ int main() {
     tests();
 
     struct instructions *i = parse(file_reader("day14/input.txt"));
-    printf("Part 1: %d\n", part1(i));
+    printf("Part 1: %ld\n", part1(i));
+    printf("Part 2: %ld\n", part2(i));
 
     free(i);
     return 0;
