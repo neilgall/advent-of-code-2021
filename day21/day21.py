1cc86c901bdebcf1f179506b50dc99a8d91beb0a
Neil Gall <neil@gall.scot>
2021-12-22 22:05:45 +0000

    Day 21 part 1

--- day21/day21.py
+++ day21/day21.py
@@ -27,6 +27,7 @@ class die:
 class player:
     def __init__(self, id, start):
         self._id = id
+        self._start = start
         self._pos = start
         self._score = 0
 
@@ -34,6 +35,20 @@ class player:
         self._pos = rollover(self._pos + n, 10)
         self._score += self._pos       
         return self._score >= 1000
+
+    def find_loop(self, die):
+        p = player(self._id, self._start)
+        pos = []
+        if self._id == 2:
+            die.roll(3) # for first player
+        while True:
+            p.move(die.roll(3))
+            pos.append(p._pos)
+            if len(pos) % 2 == 0:
+                half = pos[:len(pos)//2]
+                if pos == half + half:
+                    return half
+            die.roll(3) # for other player
 
 
 def part1(p1start, p2start):
@@ -47,11 +62,26 @@ def part1(p1start, p2start):
         if p2.move(d.roll(3)):
             return p1._score * d._rolls
 
+
+def part2(p1start, p2start):
+    p1 = player(1, p1start)
+    p2 = player(2, p2start)
+    p1_loop = p1.find_loop(die(sides=3))
+    p2_loop = p2.find_loop(die(sides=3))
+    print(p1_loop, p2_loop)
+
+
 def test_part1():
     assert part1(4, 8) == 739785
+
+
+def test_part2():
+    part2(4, 8)
 
 
 if __name__ == "__main__":
     test_part1()
     print("Part 1", part1(3, 5))
+
+    test_part2()
 
